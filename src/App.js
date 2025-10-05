cf449a0df7d5a20364eaccb62b401bf529dc916a
Pamela J Terrell <pamelajterrell@Pamelas-MacBook-Pro.local>
2025-10-04 22:15:16 -0400

    new design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'; 
 import Home from './pages/Home';  
-import Navbar from './components/Navbar';  
-import Footer from './components/Footer';  
+ 
+ 
 import './App.css'; 
 import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <div className="app">
-      <Navbar />
+      
       
       <Routes>
         <Route path="/" element={<Home />} />
         {/* add more routes as needed */}
       </Routes>
       
-      <Footer />
+    
 
       {/* Add Analytics at the root level */}
       <Analytics />
